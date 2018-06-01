3e0125453fafc5159beabd30d520e6c190794de6
Chang <cozy46th@gmail.com>
2018-06-01 21:33:12 +0800

    add cost for algorithm 1

--- src/device/Sensor.java
+++ src/device/Sensor.java
@@ -9,9 +9,12 @@ public class Sensor {
 	private static int count = 0;
 	private double x; // x coordinate
 	private double y; // y coordinate
+	private double SIOT_cost; // SIOT cost (for First Algorithm)
+	private double trans_cost; // transmission cost (for First Algorithm)
 	
 	private final Random radius = new Random();
 	private final Random angle = new Random();
+	private final Random cost = new Random();
 	
 	public Sensor() {
 		this.id = ++count;
@@ -20,6 +23,9 @@ public class Sensor {
 		double a = 2 * Math.PI * angle.nextDouble();
 		this.x = r * Math.cos(a);
 		this.y = r * Math.sin(a);
+		this.SIOT_cost = cost.nextInt(10) + 1;
+		this.trans_cost = cost.nextInt(10) + 1;
+		
 	}
 	
 	public int getID() {
@@ -34,6 +40,14 @@ public class Sensor {
 		return y;
 	}
 	
+	public double get_SIOT_cost() {
+		return SIOT_cost;
+	}
+	
+	public double get_trans_cost() {
+		return trans_cost;
+	}
+		
 	public static double calculateDistance(double x1, double y1, double x2, double y2) {
 		return Math.sqrt(Math.pow((x1 - x2), 2.0) + Math.pow((y1 - y2), 2.0));
 	}
