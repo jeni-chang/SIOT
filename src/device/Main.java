0fede685b79de090b11f1713672c796516c04ed4
Chang <cozy46th@gmail.com>
2018-06-02 21:01:36 +0800

    final

--- src/device/Main.java
+++ src/device/Main.java
@@ -1,99 +1,55 @@
 package device;
 
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
+import org.jgrapht.GraphTests;
 import org.jgrapht.alg.interfaces.SpanningTreeAlgorithm;
 import org.jgrapht.alg.interfaces.SpanningTreeAlgorithm.SpanningTree;
+import org.jgrapht.alg.interfaces.StrongConnectivityAlgorithm;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.alg.spanning.PrimMinimumSpanningTree;
+import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
-
 import graph.*;
 
-public class Main {
+public class Main{
 
 	public static void main(String[] args) {
-		/*Graph<String, DefaultWeightedEdge> g = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
 		
-		DefaultWeightedEdge e = null;
-		g.addVertex("v1");
-		g.addVertex("v2");
-		g.addVertex("v3");
-		g.addVertex("v4");
-		e = g.addEdge("v1", "v2");
-		g.setEdgeWeight(e, 4);
-//		e = g.addEdge("v2", "v3");
-//		g.setEdgeWeight(e, 5);
-		e = g.addEdge("v3", "v4");
-		g.setEdgeWeight(e, 1);
-//		e = g.addEdge("v4", "v1");
-//		g.setEdgeWeight(e, 2);
-		GraphPath<String, DefaultWeightedEdge> short_path = DijkstraShortestPath.findPathBetween(g, "v1", "v1");
-		DijkstraShortestPath<String, DefaultWeightedEdge> d = new DijkstraShortestPath<>(g);
-		System.out.println(g.degreeOf("v1"));
-		System.out.println(short_path);
-		System.out.println(short_path.getWeight());
+		/**
+		 * Algorithm 1 ----> OCGS
+		 */
+		Graph<Location, Sensor> graph_1 = new SimpleWeightedGraph<>(Sensor.class);
+		OCGSTopo OCGS_topo = new OCGSTopo();
+		graph_1 = OCGS_topo.createOCGS(2, 3);
+//		System.out.println(graph);
+//		System.out.println("Total edge number => " + graph_1.edgeSet().size());
+//		for(Sensor s : graph.edgeSet()) {
+//			System.out.println("SIOT => " + s.get_SIOT_cost() + " ,TRAN => " + s.get_trans_cost());
+//		}
+		OCGS_topo.select_candidate(graph_1);
 		
-		
-		PrimMinimumSpanningTree<String, DefaultWeightedEdge> prims = new PrimMinimumSpanningTree<>(g);
-		SpanningTree<DefaultWeightedEdge> tree;
-		tree = prims.getSpanningTree();
-		System.out.println(tree.getEdges());
-		System.out.println(tree.getWeight());*/
-		
-		
-		/*Graph<Vertex, DefaultWeightedEdge> graph = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
 		
 
-		Vertices vertices = new Vertices();
-		
-		Random random = new Random();
-		
-		for(int id = 0; id < 5; id++) {
-			Vertex sensor = new Vertex(id, Type.Sensor);
-			graph.addVertex(sensor);
-			vertices.idToVertex.put(id, sensor);
-		}
-		
-		DefaultWeightedEdge e = null;
-		
-		Object[] vertex = graph.vertexSet().toArray();
-		
-
-		Vertex RandomNode1 = (Vertex) vertex[random.nextInt(5)];
-		Vertex RandomNode2 = (Vertex) vertex[random.nextInt(5)];
-		e = graph.addEdge(RandomNode1, RandomNode2);
-
-		graph.setEdgeWeight(e, 5);
-
-		
-		
-		System.out.println(graph);
-		
-		for(Vertex v : graph.vertexSet()) {
-			System.out.println(v.getID());
-		}
-		
-		GraphPath<Vertex, DefaultWeightedEdge> short_path;
-		short_path = DijkstraShortestPath.findPathBetween(graph, RandomNode1, RandomNode2);
-		
-		System.out.println(short_path);
-		System.out.println(short_path.getWeight());*/
-		
-		Graph<Sensor, DefaultWeightedEdge> graph = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
 		
 		/**
 		 * Algorithm 2 ----> ACG
 		 */
-//		Topo ACG_topo = new Topo();
-//		graph = ACG_topo.createACG(50, 5, 15);
-////		System.out.println(graph);
-//		System.out.println("Total edge number = " + graph.edgeSet().size());
-//		System.out.println(ACG_topo.select_ACG_Center(graph));
+		
+		Graph<Sensor, DefaultWeightedEdge> graph_2 = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);	
+		ACGTopo ACG_topo = new ACGTopo();
+		//(sensor_num, basic_edge_num, edge_num)
+		graph_2 = ACG_topo.createACG(5, 2, 3);
+//		System.out.println(graph);
+//		System.out.println("Total edge number => " + graph.edgeSet().size());
+		ACG_topo.select_center(graph_2);
 		
 	}
+
 
 }
